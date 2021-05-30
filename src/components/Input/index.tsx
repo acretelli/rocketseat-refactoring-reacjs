59992ad1ec271121f6fab8920addd2241eb35112
Anna Fernandes <anna.cbf@gmail.com>
2021-05-30 16:47:25 -0300

    Adjusted the files to be config with Typescript. Converted react classes to function.

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,14 +3,20 @@ import {
   useRef,
   useState,
   useCallback,
+  InputHTMLAttributes,
 } from 'react';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+type InputProps =  {
+  name: string
+  icon?: JSX.Element
+} & InputHTMLAttributes<HTMLInputElement>
+
+const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -23,8 +29,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
 
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
-
-    setIsFilled(!!inputRef.current?.value);
+    setIsFilled(!!inputRef!.current!.value);
   }, []);
 
   useEffect(() => {
@@ -37,7 +42,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
 
   return (
     <Container isFilled={isFilled} isFocused={isFocused}>
-      {Icon && <Icon size={20} />}
+      {Icon && Icon}
 
       <input
         onFocus={handleInputFocus}
